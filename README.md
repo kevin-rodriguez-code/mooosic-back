e2d77cf4debaa1a0ee55c0c59c42494ce59c07b8
Kevin R. <kevinrc96@hotmail.com>
2024-04-22 20:52:52 +0200

    estructura basica
    Co-authored-by: AngelFG <KamekonaWTF@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mooosic-back
 back of the mooosic web
+
+- mongoose, dotenv, express
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dbConnection = require('./config/config.js')
+const app = express()
+require('dotenv').config()
+//añadir CORS
+
+const PORT = process.env.PORT || 3000
+const routes = require('./routes/')  //terminar path de routes
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/', routes);
+
+dbConnection()
+
+app.listen(PORT, () => {
+    console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
+})
