3c77240d490eeca1e5635f5226f5d21c1b88d6df
Ángel Fernández <alfgento@gmail.com>
2024-04-23 17:57:49 +0200

    db and first controllers

---
+++ routes/postsRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/Posts')
+const PostController = require('../controllers/PostController.js')
+
+router.post('/create', PostController.createPost)
+router.get('/', PostController.getAll)
+
+module.exports = router
